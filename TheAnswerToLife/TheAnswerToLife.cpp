253ccc2d4fb9c7fe88620a44ff8ecfad4894e384
mustyf10 <mustafa7_95@hotmail.com>
2020-03-20 15:21:21 +0000

    added constraints

--- TheAnswerToLife/TheAnswerToLife.cpp
+++ TheAnswerToLife/TheAnswerToLife.cpp
@@ -1,6 +1,7 @@
 // TheAnswerToLife.cpp : This file contains the 'main' function. Program execution begins and ends there.
 
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
@@ -8,13 +9,13 @@ int main()
 {
 	int n = 0;
 
-	while (n != 42)
+	while ((n != 42) && (n >= 0 && n <= numeric_limits<int>::max())) //added contraints for negative/large numbers
 	{
 		cout << "Enter an integer to find the meaning of life: ";
 		cin >> n;
+
 	}
 	cout << "Well done, you have found the meaning of life.";
 
 	return 0;
-
 }
